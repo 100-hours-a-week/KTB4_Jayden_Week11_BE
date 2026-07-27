cdab95a5e1785b8ab51e480cdbaf29ca06cdfc49
JIHYUNG KIM <martinjhkim7493@gmail.com>
2026-07-28 08:43:14 +0900

    fix: schema 추가

--- sql/schema-mysql.sql
+++ sql/schema-mysql.sql
@@ -1,75 +1,91 @@
 CREATE TABLE image_files (
                              id BIGINT NOT NULL AUTO_INCREMENT,
-                             file_path VARCHAR(2048),
+                             file_path VARCHAR(2048) NULL,
                              file_category VARCHAR(50) NOT NULL,
-                             uploader_id BIGINT,
+                             uploader_id BIGINT NULL,
                              image_status VARCHAR(50) NOT NULL,
 
                              CONSTRAINT pk_image_files PRIMARY KEY (id)
-);
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
 
 CREATE TABLE `users` (
-                         `user_id`	bigint	NOT NULL AUTO_INCREMENT,
-                         `email`	varchar(255)	NOT NULL,
-                         `password`	varchar(255)	NOT NULL,
-                         `nickname`	varchar(10)	NOT NULL,
+                         `user_id` BIGINT NOT NULL AUTO_INCREMENT,
+                         `email` VARCHAR(255) NOT NULL,
+                         `password` VARCHAR(255) NOT NULL,
+                         `nickname` VARCHAR(10) NOT NULL,
+                         `image_file_id` BIGINT NULL,
 
-                         `image_file_id` BIGINT	NULL,
-
-                         `created_at`	timestamp	NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                         `deleted_at`	timestamp	NULL DEFAULT NULL,
-                         `information_updated_at`	timestamp	NULL DEFAULT NULL,
-                         `password_updated_at`	timestamp	NULL DEFAULT NULL,
+                         `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+                         `deleted_at` DATETIME(6) NULL DEFAULT NULL,
+                         `information_updated_at` DATETIME(6) NULL DEFAULT NULL,
+                         `password_updated_at` DATETIME(6) NULL DEFAULT NULL,
 
                          CONSTRAINT `PK_USERS` PRIMARY KEY (`user_id`),
                          CONSTRAINT `UK_USERS_EMAIL` UNIQUE (`email`),
                          CONSTRAINT `UK_USERS_NICKNAME` UNIQUE (`nickname`),
 
-                         CONSTRAINT fk_users_profile_image FOREIGN KEY (image_file_id) REFERENCES image_files (id)
-);
+                         CONSTRAINT fk_users_profile_image
+                             FOREIGN KEY (`image_file_id`)
+                                 REFERENCES image_files (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
 
 CREATE TABLE `articles` (
-                            `article_id`	bigint	NOT NULL AUTO_INCREMENT,
-                            `user_id`	bigint	NOT NULL,
-                            `title`	varchar(26)	NOT NULL,
-                            `content`	mediumtext	NOT NULL,
-                            `content_images`	json	NULL,
-                            `created_at`	timestamp	NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                            `updated_at`	timestamp	NULL DEFAULT NULL,
-                            `deleted_at`	timestamp	NULL DEFAULT NULL,
-                            `is_article_hidden`	boolean	NOT NULL	DEFAULT 0,
+                            `article_id` BIGINT NOT NULL AUTO_INCREMENT,
+                            `user_id` BIGINT NOT NULL,
+                            `title` VARCHAR(26) NOT NULL,
+                            `content` MEDIUMTEXT NOT NULL,
+                            `content_images` JSON NULL,
+
+                            `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+                            `updated_at` DATETIME(6) NULL DEFAULT NULL,
+                            `deleted_at` DATETIME(6) NULL DEFAULT NULL,
+                            `is_article_hidden` BOOLEAN NOT NULL DEFAULT FALSE,
 
                             CONSTRAINT `PK_ARTICLES` PRIMARY KEY (`article_id`),
 
                             CONSTRAINT `FK_USERS_TO_ARTICLES`
                                 FOREIGN KEY (`user_id`)
                                     REFERENCES `users` (`user_id`)
-);
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
 
 CREATE TABLE `article_update_history` (
-                                          `article_history_id`	bigint	NOT NULL AUTO_INCREMENT,
-                                          `article_id`	bigint	NOT NULL,
-                                          `title`	varchar(26)	NOT NULL,
-                                          `content`	mediumtext	NOT NULL,
-                                          `content_images`	json	NULL,
-                                          `created_at`	timestamp	NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                          `article_history_id` BIGINT NOT NULL AUTO_INCREMENT,
+                                          `article_id` BIGINT NOT NULL,
+                                          `title` VARCHAR(26) NOT NULL,
+                                          `content` MEDIUMTEXT NOT NULL,
+                                          `content_images` JSON NULL,
+                                          `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
 
-                                          CONSTRAINT `PK_ARTICLE_UPDATE_HISTORY` PRIMARY KEY (`article_history_id`),
+                                          CONSTRAINT `PK_ARTICLE_UPDATE_HISTORY`
+                                              PRIMARY KEY (`article_history_id`),
 
                                           CONSTRAINT `FK_ARTICLES_TO_ARTICLE_UPDATE_HISTORY`
                                               FOREIGN KEY (`article_id`)
                                                   REFERENCES `articles` (`article_id`)
-);
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
 
 CREATE TABLE `comments` (
-                            `comment_id`	bigint	NOT NULL AUTO_INCREMENT,
-                            `article_id`	bigint	NOT NULL,
-                            `user_id`	bigint	NOT NULL,
-                            `parent_comment_id`	bigint	NULL DEFAULT NULL,
-                            `comment_text`	text	NOT NULL,
-                            `created_at`	timestamp	NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                            `updated_at`	timestamp	NULL DEFAULT NULL,
-                            `deleted_at`	timestamp	NULL DEFAULT NULL,
+                            `comment_id` BIGINT NOT NULL AUTO_INCREMENT,
+                            `article_id` BIGINT NOT NULL,
+                            `user_id` BIGINT NOT NULL,
+                            `parent_comment_id` BIGINT NULL DEFAULT NULL,
+                            `comment_text` TEXT NOT NULL,
+
+                            `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+                            `updated_at` DATETIME(6) NULL DEFAULT NULL,
+                            `deleted_at` DATETIME(6) NULL DEFAULT NULL,
 
                             CONSTRAINT `PK_COMMENTS` PRIMARY KEY (`comment_id`),
 
@@ -84,30 +100,38 @@ CREATE TABLE `comments` (
                             CONSTRAINT `FK_COMMENTS_TO_PARENT_COMMENTS`
                                 FOREIGN KEY (`parent_comment_id`)
                                     REFERENCES `comments` (`comment_id`)
-);
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
 
 CREATE TABLE `temp_articles` (
-                                 `user_id`	bigint	NOT NULL,
-                                 `title`	varchar(26)	NULL,
-                                 `content`	mediumtext	NOT NULL,
-                                 `content_images`	json	NULL,
-                                 `saved_at`	timestamp	NOT NULL	DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                                 `user_id` BIGINT NOT NULL,
+                                 `title` VARCHAR(26) NULL,
+                                 `content` MEDIUMTEXT NOT NULL,
+                                 `content_images` JSON NULL,
+                                 `saved_at` DATETIME(6) NOT NULL
+        DEFAULT CURRENT_TIMESTAMP(6)
+        ON UPDATE CURRENT_TIMESTAMP(6),
 
                                  CONSTRAINT `PK_TEMP_ARTICLES` PRIMARY KEY (`user_id`),
 
                                  CONSTRAINT `FK_USERS_TO_TEMP_ARTICLES`
                                      FOREIGN KEY (`user_id`)
                                          REFERENCES `users` (`user_id`)
-);
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
 
 CREATE TABLE `article_reports` (
-                                   `report_id`	bigint	NOT NULL AUTO_INCREMENT,
-                                   `article_id`	bigint	NOT NULL,
-                                   `user_id`	bigint	NOT NULL,
-                                   `report_type`	varchar(100)	NOT NULL	DEFAULT 'spam',
-                                   `reason`	varchar(500)	NOT NULL,
-                                   `created_at`	timestamp	NOT NULL	DEFAULT CURRENT_TIMESTAMP,
-                                   `status`	varchar(20)	NOT NULL	DEFAULT 'waiting',
+                                   `report_id` BIGINT NOT NULL AUTO_INCREMENT,
+                                   `article_id` BIGINT NOT NULL,
+                                   `user_id` BIGINT NOT NULL,
+                                   `report_type` VARCHAR(100) NOT NULL DEFAULT 'spam',
+                                   `reason` VARCHAR(500) NOT NULL,
+                                   `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+                                   `status` VARCHAR(20) NOT NULL DEFAULT 'waiting',
 
                                    CONSTRAINT `PK_ARTICLE_REPORTS` PRIMARY KEY (`report_id`),
 
@@ -121,13 +145,16 @@ CREATE TABLE `article_reports` (
 
                                    CONSTRAINT `UK_ARTICLE_REPORTS_ARTICLE_ID_USER_ID`
                                        UNIQUE (`article_id`, `user_id`)
-);
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
 
 CREATE TABLE `article_likes` (
-                                 `article_like_id`	bigint	NOT NULL AUTO_INCREMENT,
-                                 `article_id`	bigint	NOT NULL,
-                                 `user_id`	bigint	NOT NULL,
-                                 `created_at`	timestamp	NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                 `article_like_id` BIGINT NOT NULL AUTO_INCREMENT,
+                                 `article_id` BIGINT NOT NULL,
+                                 `user_id` BIGINT NOT NULL,
+                                 `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
 
                                  CONSTRAINT `PK_ARTICLE_LIKES` PRIMARY KEY (`article_like_id`),
 
@@ -141,13 +168,18 @@ CREATE TABLE `article_likes` (
 
                                  CONSTRAINT `UK_ARTICLE_LIKES_ARTICLE_ID_USER_ID`
                                      UNIQUE (`article_id`, `user_id`)
-);
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
 
 CREATE TABLE `article_views` (
-                                 `article_view_id`	bigint	NOT NULL AUTO_INCREMENT,
-                                 `article_id`	bigint	NOT NULL,
-                                 `user_id`	bigint	NOT NULL,
-                                 `updated_at`	timestamp	NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                                 `article_view_id` BIGINT NOT NULL AUTO_INCREMENT,
+                                 `article_id` BIGINT NOT NULL,
+                                 `user_id` BIGINT NOT NULL,
+                                 `updated_at` DATETIME(6) NOT NULL
+        DEFAULT CURRENT_TIMESTAMP(6)
+        ON UPDATE CURRENT_TIMESTAMP(6),
 
                                  CONSTRAINT `PK_ARTICLE_VIEWS` PRIMARY KEY (`article_view_id`),
 
@@ -161,52 +193,62 @@ CREATE TABLE `article_views` (
 
                                  CONSTRAINT `UK_ARTICLE_VIEWS_ARTICLE_ID_USER_ID`
                                      UNIQUE (`article_id`, `user_id`)
-);
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
 
 CREATE TABLE `article_stats` (
-                                 `article_id`	bigint	NOT NULL,
-                                 `comment_count`	bigint	NOT NULL	DEFAULT 0,
-                                 `article_like_count`	bigint	NOT NULL	DEFAULT 0,
-                                 `article_view_count`	bigint	NOT NULL	DEFAULT 0,
-                                 `article_report_count`	bigint	NOT NULL	DEFAULT 0,
+                                 `article_id` BIGINT NOT NULL,
+                                 `comment_count` BIGINT NOT NULL DEFAULT 0,
+                                 `article_like_count` BIGINT NOT NULL DEFAULT 0,
+                                 `article_view_count` BIGINT NOT NULL DEFAULT 0,
+                                 `article_report_count` BIGINT NOT NULL DEFAULT 0,
 
                                  CONSTRAINT `PK_ARTICLE_STATS` PRIMARY KEY (`article_id`),
 
                                  CONSTRAINT `FK_ARTICLES_TO_ARTICLE_STATS`
                                      FOREIGN KEY (`article_id`)
                                          REFERENCES `articles` (`article_id`)
-);
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
 
 CREATE TABLE refresh_token (
                                id BIGINT NOT NULL AUTO_INCREMENT,
                                token VARCHAR(255) NOT NULL,
                                user_id BIGINT NOT NULL,
-                               expires_at TIMESTAMP NOT NULL,
+                               expires_at DATETIME(6) NOT NULL,
 
                                CONSTRAINT pk_refresh_token PRIMARY KEY (id),
                                CONSTRAINT uk_refresh_token_token UNIQUE (token)
-);
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
 
 CREATE TABLE chat_rooms (
                             chat_room_id BIGINT NOT NULL AUTO_INCREMENT,
-                            created_at TIMESTAMP NOT NULL,
+                            created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
 
-                            CONSTRAINT pk_chat_rooms
-                                PRIMARY KEY (chat_room_id)
-);
+                            CONSTRAINT pk_chat_rooms PRIMARY KEY (chat_room_id)
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
 
 CREATE TABLE chat_messages (
                                chat_message_id BIGINT NOT NULL AUTO_INCREMENT,
                                chat_room_id BIGINT NOT NULL,
                                sender_id BIGINT NOT NULL,
 
-                               content text,
+                               content TEXT NULL,
                                chat_type VARCHAR(30) NOT NULL,
 
-                               created_at TIMESTAMP NOT NULL,
-                               updated_at TIMESTAMP,
-                               deleted_at TIMESTAMP,
+                               created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+                               updated_at DATETIME(6) NULL DEFAULT NULL,
+                               deleted_at DATETIME(6) NULL DEFAULT NULL,
 
                                CONSTRAINT pk_chat_messages
                                    PRIMARY KEY (chat_message_id),
@@ -218,15 +260,18 @@ CREATE TABLE chat_messages (
 
                                CONSTRAINT fk_chat_messages_sender
                                    FOREIGN KEY (sender_id)
-                                       REFERENCES users (user_id)
+                                       REFERENCES `users` (user_id)
                                        ON DELETE RESTRICT
-);
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
 
 CREATE TABLE chat_room_members (
                                    chat_room_member_id BIGINT NOT NULL AUTO_INCREMENT,
                                    chat_room_id BIGINT NOT NULL,
                                    user_id BIGINT NOT NULL,
-                                   last_read_message_id BIGINT,
+                                   last_read_message_id BIGINT NULL,
 
                                    CONSTRAINT pk_chat_room_members
                                        PRIMARY KEY (chat_room_member_id),
@@ -241,11 +286,13 @@ CREATE TABLE chat_room_members (
 
                                    CONSTRAINT fk_chat_room_members_user
                                        FOREIGN KEY (user_id)
-                                           REFERENCES users (user_id)
+                                           REFERENCES `users` (user_id)
                                            ON DELETE RESTRICT,
 
                                    CONSTRAINT fk_chat_room_members_last_read_message
                                        FOREIGN KEY (last_read_message_id)
                                            REFERENCES chat_messages (chat_message_id)
                                            ON DELETE SET NULL
-);
+) ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
