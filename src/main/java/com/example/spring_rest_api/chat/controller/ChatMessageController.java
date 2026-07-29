ca6ecf92c430dd71c0a84f9a0a60fa4ff0056fa4
JIHYUNG KIM <martinjhkim7493@gmail.com>
2026-07-29 13:52:40 +0900

    fix: 메시징 처리 컨트롤러는 RestController가 불필요

--- src/main/java/com/example/spring_rest_api/chat/controller/ChatMessageController.java
+++ src/main/java/com/example/spring_rest_api/chat/controller/ChatMessageController.java
@@ -8,11 +8,11 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class ChatMessageController {
     private final ChatService chatService;
